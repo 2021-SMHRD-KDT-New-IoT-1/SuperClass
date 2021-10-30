7e6a91e35234b6b4051388be8a8e74c0f494c787
jeon <jeon@DESKTOP-LOSJIPO>
2021-10-30 15:26:13 +0900

    id 중복체크 까지 커밋

--- test/src/com/controller/LoginCheck.java
+++ test/src/com/controller/LoginCheck.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.model.MemberDAO;
 import com.model.MemberVO;
 
 @WebServlet("/LoginCheck")
@@ -24,31 +25,13 @@ public class LoginCheck extends HttpServlet {
 		String m_id = request.getParameter("m_id");
 		String m_pw = request.getParameter("m_pw");
 		
-		Connection conn = null;
-		PreparedStatement psmt	= null;
-		ResultSet rs = null;
 		MemberVO vo = null;
+		MemberDAO dao = null;
 		
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
+		
 			
-			String url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe";
-			String dbid = "campus_a_3_1025";
-			String dbpw = "smhrd3";
-			conn = DriverManager.getConnection(url, dbid, dbpw);
-			
-			String sql = "Select * from MEMBERS where m_id = ? and m_pw = ?";
-			psmt	= conn.prepareStatement(sql);
-			psmt.setString(1, m_id);
-			psmt.setString(2, m_pw);
-			rs = psmt.executeQuery();
-			
-			if(rs.next()) {
-				String get_id = rs.getString(1);
-				String get_pw = rs.getString(2);
-				String get_name = rs.getString(3);
-				
-				vo = new MemberVO(get_id,get_pw,get_name);
+			vo = dao.login(m_id, m_pw);
+			if(vo != null) {
 				
 				HttpSession session = request.getSession();
 				
@@ -58,29 +41,8 @@ public class LoginCheck extends HttpServlet {
 			}else {
 				response.sendRedirect("Anzzi/Main.jsp");
 			}
-			
-			
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}finally {
-			//1. 지역변수
-			//2. 예외처리
-			try {
-				if(rs != null) {
-					rs.close();
-				}
-				if(psmt != null) {
-					psmt.close();
-				}
-				if(conn != null) {
-					conn.close();
-				}
-			} catch (Exception e2) {
-				e2.printStackTrace();
-			}
-			
-		}
 	}
-
 }
+		
+	
+
