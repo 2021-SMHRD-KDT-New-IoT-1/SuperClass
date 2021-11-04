de4b199b47527904685b46e063c83c30a8e41a6e
smhrd <smhrd@211.63.240.51>
2021-11-04 15:32:57 +0900

    카운팅

---
+++ test/src/com/ArdCon/Counting.java
@@ -0,0 +1,17 @@
+package com.ArdCon;
+
+public class Counting {
+	
+	public static int count = 0;
+	
+	public static int Count() {
+		return ++count;
+		
+	}
+	
+	public static void CountZero() {
+		count = 0;
+		
+	}
+	
+}
