b27702dc3cd167d5432716b7ab87a96b52fcc6dc
smhrd <smhrd@DESKTOP-TNB2RG7>
2021-11-04 17:29:22 +0900

    asdf

--- test/src/com/ArdCon/inSt.java
+++ test/src/com/ArdCon/inSt.java
@@ -2,6 +2,10 @@ package com.ArdCon;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,45 +19,51 @@ import com.google.gson.Gson;
 
 @WebServlet("/inSt")
 public class inSt extends HttpServlet {
-	
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//아두이노한테 취침시간 (버튼 눌렸을 때) 받아오는 서블릿.
-		response.setContentType("text/plain;");
-		PrintWriter out = response.getWriter();
-		
-		SensorDAO dao = new SensorDAO();
-		
-		String sleep_time = "";
-		String p_serialnum = "";
+   
+   protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		
-		sleep_time =request.getParameter("data");
-		p_serialnum= request.getParameter("p_serialnum");
-		out.println(sleep_time);
-		out.println(p_serialnum);
-		
-		
-	
-		if(sleep_time.equals("off")) {
-			System.out.println("inSt.java에서 값을 못 받아 오거나 없음");
-			System.out.println("값이 들어옴");
-			System.out.println("서버로 들어온 값 : " + sleep_time);
-			
-			////이부분을 바꿔야함 SensorDAO에서. 아직 안 만듦.
-			int cnt = dao.inSt(sleep_time, p_serialnum);
-			
-			if(cnt>0) {
-				System.out.println("sleep_time insert 성공!");
-			}else {
-				System.out.println("sleep_time insert 실패!");
-			}
-			
-		} else {
-			System.out.println("inSt값 null");
-		
-		}
-			
-		
-	}
+      PrintWriter out = response.getWriter();
+      
+      SensorDAO dao = new SensorDAO();
+      
+      
+      String p_serialnum = "";
+
+      
+      String sleep_time =request.getParameter("data");
+      p_serialnum= request.getParameter("p_serialnum");
+      
+      if(sleep_time == null) {
+         sleep_time = "";
+      }
+      
+      if(sleep_time.equals("on")) {
+         //아두이노한테 취침시간 (버튼 눌렸을 때) 받아오는 서블릿.
+         LocalTime now = LocalTime.now();
+         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH시 mm분 ss초");
+         // 포맷 적용하기
+         String formatedNow = now.format(formatter);
+         
+         System.out.println("값이 들어옴");
+         System.out.println("서버로 들어온 값 : " + sleep_time);
+         System.out.println("DB에 넣을 시간"  + formatedNow);
+         
+         ////이부분을 바꿔야함 SensorDAO에서. 아직 안 만듦.
+         
+         int cnt = dao.inSt(formatedNow, p_serialnum);
+         
+         if(cnt>0) {
+            System.out.println("sleep_time insert 성공!");
+         }else {
+            System.out.println("sleep_time insert 실패!");
+         }
+         
+      } else {
+         System.out.println("inSt값 null");
+      
+      }
+         
+      
+   }
 
 }
